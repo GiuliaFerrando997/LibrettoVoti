439d7cb44fa5bf30e719a087c5fce6607246422e
Giulia <Giulia@DESKTOP-4GDMIII>
2019-03-25 13:43:50 +0100

    Iniziato il punto 4

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -28,5 +28,34 @@ public class Libretto {
 		}
 		return result;
 	}
+	
+	/**
+	 * Ricerca il voto relativo al corso specificato
+	 * @param nomeEsame nome del corso da cercare
+	 * @return il voto se disponibile
+	 */
+	public Voto cercaEsame(String nomeEsame) {
+		for(Voto v : this.voti) {
+			if(v.getCorso().equals(nomeEsame))
+				return v;
+		} 
+		return null;
+	}
+	
+	/**
+	 * Dato un {@link Voto} determina se esiste già un voto con
+	 * uguale corso e punteggio
+	 * @param v
+	 * @return vero se voto e esame uguali, altrimenti falso
+	 */
+	public boolean esisteGiaVoto(Voto v) {
+		Voto trovato = this.cercaEsame(v.getCorso());
+		if(trovato==null)
+			return false;
+		if(trovato.getPunti()==v.getPunti())
+			return true;
+		else
+			return false;
+	}
 
 }
